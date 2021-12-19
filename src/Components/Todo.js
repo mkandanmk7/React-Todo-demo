27993c15050102e28cdcbbaa512139e987db5391
mkandanmk7 <mkandanmk7@gmail.com>
2021-12-19 15:33:13 +0530

    deleted

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,10 +1,15 @@
+import { Backspace, Edit } from "@mui/icons-material";
 import React from "react";
 
-const Todo = ({ todos }) => {
+const Todo = ({ todos, deleteTodo }) => {
   return (
     <div className="singleTodo">
       {todos.map((todo) => (
-        <h1>{todo.text}</h1>
+        <div key={todo.id} className="singleTodo">
+          <h1>{todo.text}</h1>
+          <Edit />
+          <Backspace onClick={() => deleteTodo(todo.id)} />
+        </div>
       ))}
     </div>
   );
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -12,14 +12,26 @@ const TodoList = () => {
     }
     const newTodoList = [todo, ...todos];
 
+    console.log("todo list array:", newTodoList);
+
     //change new todo
     setTodos(newTodoList);
   };
+
+  const deleteTodo = (id) => {
+    console.log("deleted id:", id);
+    const deletedTodos = [...todos].filter((todo) => todo.id !== id);
+
+    //change the list
+
+    setTodos(deletedTodos);
+  };
+
   return (
     <div>
       <TodoForm onSubmit={addTodo} />
       {/* send list of todos to todo components */}
-      <Todo todos={todos} />
+      <Todo todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
 };
